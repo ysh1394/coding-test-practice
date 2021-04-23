50a5ac2e3171fb7940420c3cd5a6725da4eb69fc
ysh1394 <ysh13941394@gmail.com>
2021-04-23 16:14:15 +0900

    Add: inflearn 알고리즘 공부내용 최초 커밋

---
+++ inflearn/배열 홀수 찾기, 총합, 최소값.js
@@ -0,0 +1,62 @@
+// 7개의 자연수가 주어질 때, 이들 중 홀수인 자연수들을 모두 골라 그 합을 구하고,
+// 고른 홀수들 중 최소값을 찾는 프로그램을 작성하세요.
+// 예를 들어, 7갸의 자연수 12, 77, 38, 41, 53, 92, 85가 주어지면
+// 이들 중 홀수는 77, 41, 53, 85 이므로 그 합은
+//   77 + 41 + 53 + 85 = 256
+// 이 되고,
+//   41 < 53 < 77 < 85
+// 이므로 홀수들 중 최소값은 41이 된다.
+
+// * 입력 설명
+// 첫번째 줄에 자연수 7개가 주어진다. 주어지는 자연수는 100보다 작다.
+// 홀수가 한개이상 반드시 존재한다.
+
+// * 출력 설명
+// 첫째 줄에 홀수들의 합을 출력하고, 둘째 줄에 홀수들 중 최소값을 출력한다.
+
+// * 입력예제 1
+// 12 77 38 41 53 92 85
+
+// * 출력예제 1
+// 256
+// 41
+
+// 내가 푼 답
+function myAnswer (x) {
+  let answer = []
+  const arrFilter = x.filter(f => f % 2 === 1)
+  const arrSum = arrFilter.reduce((total, currValue) => {
+    return total + currValue
+  })
+  const arrSort = arrFilter.sort((a,b) => {
+    return a - b
+  })
+  
+  return answer = [arrSum, arrSort[0]]
+}
+
+x = [12, 77, 38, 41, 53, 92, 85]
+console.log('myAnswer(x) >>>', myAnswer(x))
+
+
+
+// 강의 답
+function solution (arr) {
+  let answer = []
+  let sum=0, min=Number.MAX_SAFE_INTEGER
+  // Number.MAX_SAFE_INTEGER = 숫자 중에서 가장 큰 값
+  
+  for (let x of arr) {
+    if (x % 2 === 1) {
+      sum += x
+      if (x < min) {
+        min = x
+      }
+    }
+  }
+  answer.push(sum)
+  answer.push(min)
+  return answer
+}
+
+console.log('solution(x) >>>', solution(x))
