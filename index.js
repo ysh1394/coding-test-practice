0ae221c1f1a6bb4763bbdbbb17f8560961a3b4d5
ysh1394 <ysh13941394@gmail.com>
2021-12-16 01:45:58 +0900

    feat: 터미널에서 아웃풋 확인 가능하도록 수정

---
+++ index.js
@@ -0,0 +1,10 @@
+const output = require('./src/index');
+
+const express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
+const app = express(); //express를 실행하여 app object를 초기화 합니다.
+
+const port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다.
+app.listen(port, function () {
+  // 출력값 확인 하는 곳
+  return output;
+});
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// 각 폴더에 있는 문제들 중 풀고자 하는 문제를 여기서 테스트 해볼 수 있음
+
+const solution = () => {
+  let answer; // 삭제 금지
+
+  console.log('output >>>>', answer); // 삭제 금지
+};
+
+const output = solution();
+
+exports.output = output;
