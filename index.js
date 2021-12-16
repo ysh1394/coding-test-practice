8a1ad8be30d9d2fc964f15a469551e5bf8ee9f82
ysh1394 <ysh1394@github.com>
2021-12-17 01:09:48 +0900

    ssh test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express'); // 설치한 express module을 불러와서 
 const app = express(); //express를 실행하여 app object를 초기화 합니다.
 
 const port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다.
-app.listen(port, function () {
-  // 출력값 확인 하는 곳
+app.listen(port, function() {
+  // 출력값(console.log) 확인 하는 곳
   return output;
 });
